963cc4e7e1d5db7af88c8ba89b60357e4691939b
Claudio Guevara <claudio.guevara@pharmkt.com>
2025-05-24 13:45:10 -0400

    boton para cerrar todas las pestañas y quitamos botones del sidebar

--- src/renderer/src/components/playground/PlaygroundSidebar.tsx
+++ src/renderer/src/components/playground/PlaygroundSidebar.tsx
@@ -1,11 +1,8 @@
-import { Button, Divider, useDisclosure } from '@heroui/react'
 import { Tree } from '@renderer/interfaces/playground'
 import { JSX, useState } from 'react'
 import TreeView, { flattenTree } from 'react-accessible-treeview'
-import { IoMdSettings } from 'react-icons/io'
 import { v4 as uuidv4 } from 'uuid'
 
-import SettingsModal from '../general/SettingsModal'
 import PlaygroundSidebarItem from './PlaygroundSidebarItem'
 
 function PlaygroundSidebar(): JSX.Element {
@@ -36,48 +33,27 @@ function PlaygroundSidebar(): JSX.Element {
     ]
   })
 
-  const {
-    isOpen: isOpenSettings,
-    onOpen: onOpenSettings,
-    onOpenChange: onOpenChangeSettings,
-    onClose: onCloseSettings
-  } = useDisclosure()
-
   const data = flattenTree(tree)
 
   return (
-    <>
-      <div className="flex justify-between h-full flex-col border-r dark:border-default-200">
-        <div className="h-full">
-          <TreeView
-            className="pt-3 px-1.5"
-            data={data}
-            nodeRenderer={({ element, getNodeProps, level, handleExpand }) => (
-              <PlaygroundSidebarItem
-                element={element}
-                getNodeProps={getNodeProps}
-                level={level}
-                tree={tree}
-                setTree={setTree}
-                handleExpand={handleExpand}
-              />
-            )}
-          />
-        </div>
-        <Divider />
-        <div className="p-3 flex items-center justify-center gap-2">
-          <Button isIconOnly variant="bordered" onPress={onOpenSettings}>
-            <IoMdSettings className="w-5 h-5" />
-          </Button>
-        </div>
+    <div className="flex justify-between h-full flex-col border-r dark:border-default-200">
+      <div className="h-full">
+        <TreeView
+          className="pt-3 px-1.5"
+          data={data}
+          nodeRenderer={({ element, getNodeProps, level, handleExpand }) => (
+            <PlaygroundSidebarItem
+              element={element}
+              getNodeProps={getNodeProps}
+              level={level}
+              tree={tree}
+              setTree={setTree}
+              handleExpand={handleExpand}
+            />
+          )}
+        />
       </div>
-
-      <SettingsModal
-        isOpen={isOpenSettings}
-        onOpenChange={onOpenChangeSettings}
-        onClose={onCloseSettings}
-      />
-    </>
+    </div>
   )
 }
 
--- src/renderer/src/routes/playground.tsx
+++ src/renderer/src/routes/playground.tsx
@@ -1,4 +1,5 @@
-import { Chip, Tab, Tabs } from '@heroui/react'
+import { Button, Chip, Tab, Tabs, useDisclosure } from '@heroui/react'
+import SettingsModal from '@renderer/components/general/SettingsModal'
 import ConnectionList from '@renderer/components/home/ConnectionList'
 import HomeMenu from '@renderer/components/home/HomeMenu'
 import Playground from '@renderer/components/playground/Playground'
@@ -6,6 +7,8 @@ import { useConnectionsStore } from '@renderer/store/connectionsStore'
 import { createFileRoute, useNavigate, useSearch } from '@tanstack/react-router'
 import { JSX, useEffect, useState } from 'react'
 import { FaPlus } from 'react-icons/fa'
+import { IoMdSettings } from 'react-icons/io'
+import { VscCloseAll } from 'react-icons/vsc'
 
 interface RouteSearch {
   id?: string
@@ -28,6 +31,12 @@ function RouteComponent(): JSX.Element {
   const { connections, handleConnections } = useConnectionsStore()
   const navigate = useNavigate()
   const search = useSearch({ from: '/playground' })
+  const {
+    isOpen: isOpenSettings,
+    onOpen: onOpenSettings,
+    onOpenChange: onOpenChangeSettings,
+    onClose: onCloseSettings
+  } = useDisclosure()
 
   useEffect(() => {
     const filterConnections = connections.filter((connection) => connection.active)
@@ -60,42 +69,68 @@ function RouteComponent(): JSX.Element {
     }
   }
 
-  return (
-    <div className="h-full min-h-screen flex flex-col pt-2 px-2">
-      <Tabs
-        aria-label="connections"
-        variant="light"
-        size="lg"
-        color="primary"
-        selectedKey={selectedConnection}
-        onSelectionChange={setSelectedConnection}
-      >
-        {connections
-          .filter((connection) => connection.active)
-          .map((connection) => (
-            <Tab
-              key={connection.id}
-              title={
-                <Chip
-                  className={`bg-transparent ${connection.id === selectedConnection ? 'text-white ' : 'text-gray-900 dark:text-white'}`}
-                  onClose={() => handleDesactiveConnection(connection.id)}
-                >
-                  {connection.name}
-                </Chip>
-              }
-              className="flex-1 flex"
-            >
-              <Playground />
-            </Tab>
-          ))}
+  const closeAllTabs = (): void => {
+    const newConnections = connections.map((connection) => ({ ...connection, active: false }))
 
-        <Tab title={<FaPlus className="w-4 h-4" />}>
-          <div className="p-3 space-y-4">
-            <HomeMenu isPlayground={true} />
-            <ConnectionList isPlayground={true} />
-          </div>
-        </Tab>
-      </Tabs>
-    </div>
+    localStorage.setItem('connections', JSON.stringify(newConnections))
+    handleConnections(newConnections)
+
+    navigate({ to: '/' })
+  }
+
+  return (
+    <>
+      <div className="h-full min-h-screen flex flex-col pt-2 px-2">
+        <div className="mb-2 flex items-center justify-start pl-1 gap-2">
+          <Button isIconOnly variant="bordered" onPress={closeAllTabs}>
+            <VscCloseAll className="w-6 h-6" />
+          </Button>
+          <Button isIconOnly variant="bordered" onPress={onOpenSettings}>
+            <IoMdSettings className="w-5 h-5" />
+          </Button>
+        </div>
+
+        <Tabs
+          aria-label="connections"
+          variant="light"
+          size="lg"
+          color="primary"
+          selectedKey={selectedConnection}
+          onSelectionChange={setSelectedConnection}
+        >
+          {connections
+            .filter((connection) => connection.active)
+            .map((connection) => (
+              <Tab
+                key={connection.id}
+                title={
+                  <Chip
+                    className={`bg-transparent ${connection.id === selectedConnection ? 'text-white ' : 'text-gray-900 dark:text-white'}`}
+                    onClose={() => handleDesactiveConnection(connection.id)}
+                  >
+                    {connection.name}
+                  </Chip>
+                }
+                className="flex-1 flex"
+              >
+                <Playground />
+              </Tab>
+            ))}
+
+          <Tab title={<FaPlus className="w-4 h-4" />}>
+            <div className="p-3 space-y-4">
+              <HomeMenu isPlayground={true} />
+              <ConnectionList isPlayground={true} />
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
+
+      <SettingsModal
+        isOpen={isOpenSettings}
+        onOpenChange={onOpenChangeSettings}
+        onClose={onCloseSettings}
+      />
+    </>
   )
 }
